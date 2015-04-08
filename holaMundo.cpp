182d060c21aa6d22d35e0655810ec95f9b028520
LucianaNapolillo <luciananapolillo@gmail.com>
2015-04-08 10:02:44 -0300

    Subo TP #0 - Hola Mundo

---
+++ holaMundo.cpp
@@ -0,0 +1,13 @@
+/* Muestra un mensaje por pantalla'
+* Luciana Napolillo
+* 08/04/2015
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "Hola Mundo! \u2665" << endl;
+	return 0;
+}
